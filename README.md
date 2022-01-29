c74c169883749662d0b6593800b7bea4378bba3b
tbsmcd <tbsmcd@gmail.com>
2022-01-29 22:06:28 +0900

    update readme

--- README.md
+++ README.md
@@ -1,11 +1,86 @@
 # youtube_search_practice
+## 基本方針
+- Google 製 client は使いません
+- OAuth は不要なので API Key を使用します
+- コマンドとしてパイプラインでつなぎやすい設計にします
+    - 標準出力・標準エラー出力を使い分けます
+    - API Key が存在しないときに対話的に入力を求めるのではなく、標準エラー出力を返します
+    - API Key の保存にはサブコマンドを用意します
+    - json 等での出力よりも、まずはテキストを優先しました
 
-- Google 製 client は使わない
+## 動作環境
+付属の docker-compose で動作を確認しています。
 
-## Start to use
-
+起動
 ```bash
 $ docker-compose up -d --build
-$ docker-compose exec youtube_py bash 
 ```
 
+コンテナに入る
+```bash
+$ docker-compose exec youtube_py bash
+```
+
+コマンドの実行
+```bash
+# python scripts/main.py search "エレファント"
+https://www.youtube.com/watch?v=0melyLHqP-s 今宵の月のように/エレファントカシマシ
+https://www.youtube.com/watch?v=tIe-kXTmj3E 椎名林檎と宮本浩次－獣ゆく細道
+https://www.youtube.com/watch?v=0WZu7L7Hjds 悲しみの果て ≡ エレファントカシマシ
+https://www.youtube.com/watch?v=cWHU2-WnZqc エレファントカシマシ - 「俺たちの明日」
+https://www.youtube.com/watch?v=2XZvRqpqsnw エレファントカシマシ「ズレてる方がいい」
+https://www.youtube.com/watch?v=AVPbDnPiYac 翳りゆく部屋　エレファントカシマシcover
+https://www.youtube.com/watch?v=jHmpt5et9bk エレファントカシマシ - 笑顔の未来へ
+https://www.youtube.com/watch?v=fa-MBPOeQbM エレファントカシマシ - 「桜の花、舞い上がる道を」
+https://www.youtube.com/watch?v=3AjX6dkMWyE エレファントカシマシ「RAINBOW」
+https://www.youtube.com/watch?v=Ms7Ec594Xz4 エレファントカシマシ / 俺たちの明日
+
+```
+
+## 使い方
+
+まずは YouTube API Key を取得して、
+```bash
+# python scripts/main.py key [API_KEY]
+```
+を実行して Key を保存します。  
+API Key が保存されていなかったり誤った Key が保存されている場合に検索を実行してもエラーメッセージで案内します。  
+  
+検索は
+```bash
+# python scripts/main.py search "音楽"
+```
+のように行います。デフォルトの検索件数は10件ですが、オプションで件数を指定することもできます。（1 <= num <= 50）
+```bash
+# python scripts/main.py search "音楽" --num 20
+```
+
+詳しくは help を参照して下さい。
+```bash
+# python scripts/main.py --help
+Usage: main.py [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  key     API Key を保存するサブコマンド。引数に API Key を入力して下さい。
+  search  検索する場合のサブコマンド。引数にスペースが含まれる場合はダブルクォーテーションで囲って下さい
+  
+# python scripts/main.py search --help
+Usage: main.py search [OPTIONS] KEYWORD
+
+  検索する場合のサブコマンド。引数にスペースが含まれる場合はダブルクォーテーションで囲って下さい
+
+Options:
+  --num INTEGER RANGE  表示件数を入力して下さい。  [1<=x<=50]
+  --help               Show this message and exit.
+
+# python scripts/main.py key --help
+Usage: main.py key [OPTIONS] KEY
+
+  API Key を保存するサブコマンド。引数に API Key を指定して下さい。
+
+Options:
+  --help  Show this message and exit.
+```
--- scripts/main.py
+++ scripts/main.py
@@ -24,9 +24,10 @@ def confirm_key() -> str:
     try:
         api_key = config['Default']['key']
     except KeyError:
-        print('API Key が存在しません。サブコマンド `main.py key [API KEY]` で保存できます。')
-        print('下記リンクから取得して入力して下さい。')
-        print('https://console.cloud.google.com/apis/credentials?project=upheld-rookery-339704&supportedpurview=project')
+        mess = 'API Key が存在しません。サブコマンド `main.py key [API KEY]` で保存できます。'\
+            + '次の URL から取得して入力して下さい。'\
+            + 'https://console.cloud.google.com/apis/credentials?project=upheld-rookery-339704&supportedpurview=project'
+        click.echo(mess, err=True)
         sys.exit()
     return api_key
 
@@ -38,20 +39,26 @@ def main(ctx):
         print(ctx.get_help())
 
 
-@main.command(help='API Key を保存するサブコマンド。引数に API Key を入力して下さい。')
+@main.command(help='API Key を保存するサブコマンド。引数に API Key を指定して下さい。')
 @click.argument('key', type=str, required=True)
-def key(key: str) -> str:
-    config = configparser.ConfigParser()
-    config['Default'] = {'key': key}
-    with open(ini_file, 'w') as configfile:
-        config.write(configfile)
+def key(key: str):
+    try:
+        config = configparser.ConfigParser()
+        config['Default'] = {'key': key}
+        with open(ini_file, 'w') as configfile:
+            config.write(configfile)
+            click.echo(key)
+    except Exception:
+        # TODO: 例外の幅を広く取りすぎているから検討
+        click.echo('failed', err=True)
 
 
 @main.command(help='検索する場合のサブコマンド。引数にスペースが含まれる場合はダブルクォーテーションで囲って下さい')
 @click.argument('keyword', type=str, required=True)
-def search(keyword: str):
+@click.option('--num', type=click.IntRange(1, 50), required=False, default=10,
+              help='表示件数を入力して下さい。')
+def search(keyword: str, num: int):
     api_key = confirm_key()
-    print(keyword)
     endpoint = 'https://www.googleapis.com/youtube/v3/search'
     # 空白は + に置換して検索する
     params = {
@@ -59,15 +66,19 @@ def search(keyword: str):
         'part': 'snippet',
         'type': 'video',
         'order': 'viewCount',
+        'maxResults': num,
         'q': '+'.join(keyword.split()),
     }
-    print(params)
     r = requests.get(endpoint, params=params)
-    if r.status_code == 400:
-        # API Key が不正
-        print('API Key が不正なようです。')
-        # 再帰的に処理しても良いが単純なコマンドなので終了する
-        sys.exit()
+    if r.status_code == 200:
+        video_base_url = 'https://www.youtube.com/watch?v='
+        for item in r.json()['items']:
+            click.echo(video_base_url + item['id']['videoId'] + ' ' + item['snippet']['title'])
+
+    elif r.status_code == 400:
+        click.echo('API Key が不正なようです。サブコマンド key を使って下さい。', err=True)
+    else:
+        click.echo('原因不明のエラー http status code: {0}'.format(r.status_code), err=True)
 
 
 if __name__ == '__main__':
